f09770723ff1ce9d0ea1f62770267de98b85bd78
Ramesh Sampath <ramesh@sampathweb.com>
2022-05-21 14:04:46 -0700

    Update trusted_partners.js

--- .github/workflows/trusted_partners.js
+++ .github/workflows/trusted_partners.js
@@ -37,8 +37,8 @@ const intel_action = async ({github, context}) => {
   const labels = ['bug', 'ready-to-pull'];
   const assignees = ['shobanavv'];
   console.log("BEGIN");
-  console.log(context.title);
-  console.log(context.issue.title);
+  console.log(context.issue);
+  console.log(context);
   console.log("END");
   const resp_label = await github.rest.issues.addLabels({
     issue_number: context.issue.number,
